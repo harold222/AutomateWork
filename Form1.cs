e29f496c822fe28426b4f0e20454ee13c5615839
Harold Pedraza <32584758+harold222@users.noreply.github.com>
2022-10-13 08:53:36 -0500

    -

--- Form1.cs
+++ Form1.cs
@@ -1,7 +1,11 @@
+using System.Reflection;
+
 namespace automateWork
 {
     public partial class Form1 : Form
     {
+        private readonly string pathConfig = $"{Path.GetDirectoryName(Assembly.GetEntryAssembly().Location)}/config.json";
+
         public Form1()
         {
             InitializeComponent();
@@ -9,7 +13,15 @@ namespace automateWork
 
         private void initProcess_Click(object sender, EventArgs e)
         {
+            string currentDate = DateTime.Now.ToString("dd-mm-yyyy");
+            if (File.Exists(this.pathConfig))
+            {
 
+            }
+            else
+            {
+                MessageBox.Show("El archivo de configuracion config.json no existe", "Configuracion carpeta", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
         }
 
         private void stopProcess_Click(object sender, EventArgs e)
